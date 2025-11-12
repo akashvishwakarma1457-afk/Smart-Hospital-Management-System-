2962a86a81eca8af182eaa7899767cb7caa63142
Akash Vishwakarma <akashavishwakarma1457@gmail.com>
2025-11-12 16:00:32 +0530

    Remove the  name

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -21,13 +21,6 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div>
-          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-          <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+1-212-456-7890</li>
-            <li>pritamchaudhary@gmail.com</li>
-          </ul>
-        </div>
 
       </div>
 
